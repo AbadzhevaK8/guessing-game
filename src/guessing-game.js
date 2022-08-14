d38c4457f57c8769dbebc6a2fa68a6656e9c43b5
k8 <make.me.loco@gmail.com>
2022-08-14 12:40:49 +0300

    feat: add guessing game logic.

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.resGuess = 0;
     }
 
     guess() {
-
+        this.resGuess = this.max - Math.floor((this.max - this.min) / 2);
+        return this.resGuess;
     }
 
     lower() {
-
+        this.max = this.resGuess;
     }
 
     greater() {
-
+        this.min = this.resGuess;
     }
 }
 
